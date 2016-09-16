5b9dc98a153e6d76c99c6630e32f39c439aba9e1
Martín Lobo <kefala@MacBook-Pro-de-Martin.local>
2016-09-16 16:07:50 -0300

    Add passport dependecies and routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+	clientID: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET,
+	callbackURL: "http://www.example.com/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+	User.findOrCreate({ googleId: profile.id }, function (err, user) {
+		return done(err, user);
+	});
+}
+));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Login/login', {});
+	res.render('Login/login', {});
+});
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+	res.redirect('/');
 });
 
 module.exports = router;

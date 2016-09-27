286e71302cfd1358b1b12f597276420c08c39f5f
Martín Lobo <kefala@MacBook-Pro-de-Martin.local>
2016-09-27 16:07:18 -0300

    Dev react enviroment config

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 "use strict";
 
 var gulp = require('gulp');
+
 var reactify = require('reactify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
+var concat = require('gulp-concat');
+
+var lint = require('gulp-eslint');
 
 var config = {
 	paths: {
-		js: ['app/main.js'],
+		mainJs: 'app/main.js',
+		js: ['app/**/*.js'],
 		jsOut: 'public/static/js/'
 	}
 };
@@ -17,10 +22,21 @@ var config = {
 gulp.task('default', ['js']);
 
 gulp.task('js', function () {
-	browserify(config.paths.js)
+	browserify(config.paths.mainJs)
 	.transform(reactify)
 	.bundle()
 	.on('error', console.error.bind(console))
 	.pipe(source('bundle.js'))
 	.pipe(gulp.dest( config.paths.jsOut ));
 });
+
+gulp.task('lint', function () {
+	return gulp.src(config.paths.js)
+	.pipe(lint())
+	.pipe(lint.format());
+});
+
+gulp.task('watch', function () {
+	gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
